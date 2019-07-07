3de794da54abe41ceebd6c0ccb64a625f7d81670
Margular <2609135351@qq.com>
2019-07-07 21:38:23 +0800

    add hooking following apps feature

--- android/hook.py
+++ android/hook.py
@@ -3,19 +3,20 @@
 
 import frida
 import os
-import sys
+import re
 import time
 
-PROCESS = 'io.github.margular'
 LOG_PATH = os.path.join('logs', time.ctime()).replace(':', '_') + '.txt'
+
 
 def append_log(log_path, text):
     log_dir = os.path.split(log_path)[0]
-    os.makedirs(log_dir, mode = 0o700, exist_ok = True)
-    with open(log_path, 'a', encoding = 'utf8') as f:
+    os.makedirs(log_dir, mode=0o700, exist_ok=True)
+    with open(log_path, 'a', encoding='utf8') as f:
         f.write(text)
         f.write(os.linesep)
     print(text)
+
 
 def on_message(message, data):
     if message['type'] == 'error':
@@ -27,8 +28,39 @@ def on_message(message, data):
 
     append_log(LOG_PATH, text)
 
-def main():
-    process = frida.get_usb_device().attach(PROCESS)
+
+# hook all following started apps automatically for hands free
+# hook_regexp: the app name regexp you want to hook with
+# init_hook_regexp: the app name regexp you want to init hook before hook following apps
+def hook_following_apps(hook_regexp=None, init_hook_regexp=None):
+    # recording all apps first
+    name_pool = set(_.name for _ in frida.get_usb_device().enumerate_processes())
+
+    # init hook
+    if init_hook_regexp:
+        for name in name_pool:
+            for _ in init_hook_regexp:
+                if re.search(_, name):
+                    print('init hook with <' + name + '>')
+                    hook(name)
+                    break
+
+    while True:
+        time.sleep(0.1)
+        name_pool_2 = set(_.name for _ in frida.get_usb_device().enumerate_processes())
+        differ_names = name_pool_2 - name_pool
+        for name in differ_names:
+            time.sleep(2)
+            print('Now hook <' + name + '>')
+            try:
+                hook(name)
+            except (frida.ProcessNotFoundError, frida.TransportError) as e:
+                print(e)
+        name_pool = name_pool_2
+
+
+def hook(name):
+    process = frida.get_usb_device().attach(name)
     js = 'Java.perform(function() {'
     for (dirpath, dirnames, filenames) in os.walk('scripts'):
         for filename in filenames:
@@ -38,7 +70,11 @@ def main():
     script.on('message', on_message)
     print('[*] All code activated!')
     script.load()
-    sys.stdin.read()
+
+
+def main():
+    hook_following_apps(init_hook_regexp=['^com\.huawei\.'])
+
 
 if __name__ == '__main__':
     main()
