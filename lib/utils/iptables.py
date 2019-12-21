6f18265e6f6c84a2589d289c7db368999e3681ae
Margular <2609135351@qq.com>
2019-12-21 14:43:52 +0800

    Enable mobile data traffic to be captured

--- lib/utils/iptables.py
+++ lib/utils/iptables.py
@@ -3,14 +3,16 @@
 
 
 class Iptables:
-    install_cmd = 'iptables -t nat -A OUTPUT -p tcp -o wlan0 -j DNAT --to-destination 127.0.0.1:{}'
+    install_cmd = 'iptables -t nat -A OUTPUT -p tcp -o lo -j RETURN'
+    install_cmd2 = 'iptables -t nat -A OUTPUT -p tcp -j DNAT --to-destination 127.0.0.1:{}'
     uninstall_cmd = 'iptables -t nat -F'
 
     def __init__(self, adb):
         self._adb = adb
 
     def uninstall(self):
-        return self._adb.unsafe_shell(self.uninstall_cmd, root=True)
+        self._adb.unsafe_shell(self.uninstall_cmd, root=True)
 
     def install(self, port):
-        return self._adb.unsafe_shell(self.install_cmd.format(port), root=True)
+        self._adb.unsafe_shell(self.install_cmd, root=True)
+        self._adb.unsafe_shell(self.install_cmd2.format(port), root=True)
