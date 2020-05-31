8f8d71cbdfa07d643d9bdc71a4753fc81545ce8c
Margular <2609135351@qq.com>
2020-05-31 22:21:02 +0800

    更详尽的说明

--- lib/core/options.py
+++ lib/core/options.py
@@ -6,17 +6,21 @@ import argparse
 
 class Options:
     def __init__(self):
-        parser = argparse.ArgumentParser(description='A tool that hook all apps you need')
+        parser = argparse.ArgumentParser(description='a tool that helps you hook the program you want to hook '
+                                                     'according to regular expressions, more details see: '
+                                                     'https://github.com/Margular/frida-skeleton')
 
         parser.add_argument('regexps', nargs='*', default=[r'^com\.'],
-                            help=r'Regexps for the apps you want to hook such as "^com\.baidu\.", '
-                                 r'empty for hooking all apps')
+                            help=r'regular expressions that specifies the application names you want to hook, for '
+                                 r'example "^com\.baidu\.", if it is empty, hook all programs starting with com.')
         parser.add_argument('-i', '--install', action='store_true',
                             help='install frida server to /data/local/tmp automatically')
         parser.add_argument('-p', '--port', type=int,
-                            help='reverse tcp port, if specified, manipulate iptables automatically')
+                            help='reverse tcp port, if specified, manipulate iptables automatically, data flow: '
+                                 'mobile | all tcp streams -> mobile | tcp 8080 -> your pc/laptop | tcp 8080, done '
+                                 'by iptables and adb reverse')
         parser.add_argument('-s', '--spawn', action='store_true',
-                            help='spawn mode on, attach mode off')
+                            help='spawn mode on, attach mode off, same as native frida')
         parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
 
         args = parser.parse_args()
