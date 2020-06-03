63a164daabb109ba7f593cfa1d8821eeb1e39d33
Margular <2609135351@qq.com>
2020-06-03 21:35:44 +0800

    添加注释，后期修复

--- scripts/utils/format.js
+++ scripts/utils/format.js
@@ -22,6 +22,7 @@ var Format = {
         if (obj.__proto__ === Java.array('byte', []).__proto__) {
             var str = Java.use('java.lang.String').$new(obj).toString();
             var validStr = str.replace(/[\x00-\x09\x0B-\x0C\x0E-\x1F\x7F-\x9F]/g, '');
+            // need fix: the regexp can not work perfectly
             if (str.length > validStr.length)
                 return Conversion.bytes2hex(obj);
             else
